6734b6b45b487683cf5a7aea9dafd31bb9d8377b
meekhumor <om17092004@gmail.com>
2025-02-27 18:54:44 +0530

    Update app to use environment variable for port configuration

--- app.py
+++ app.py
@@ -90,4 +90,6 @@ def process_video():
     })
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port)
